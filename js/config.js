98abd7075b0511425217994216afb14b17f5394a
Chetan J <jilladwarchetan@gmail.com>
2026-01-13 21:23:00 +0530

    Initial project upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// js/config.js (NEW FILE)
+// Yeh file aapki API URL ko globally define karegi
+// Is file ko har HTML page mein sabse pehle link karein.
+
+var GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwPeH_oXus5h88Y1H08AoMRNgSIaaZB_sX5Xuu2MT1BAFBILF_DhB3yVEX9nW7v0ozbHw/exec"; // Aapka URL
